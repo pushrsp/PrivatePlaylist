642ab852d637d58fca394a1fa614b651d6936bcb
PUSHRSP <pushrsp@gmail.com>
2023-02-18 18:47:20 +0900

    #7 - 테마 설정 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,22 @@
-function App() {
-    return <div className="app"></div>
+import React from 'react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+import { ColorModeContext, useMode } from './config/theme'
+
+const App = () => {
+    const [theme, colorMode] = useMode()
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline>
+                    <div className="app">
+                        <main className="content"></main>
+                    </div>
+                </CssBaseline>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    )
 }
 
 export default App

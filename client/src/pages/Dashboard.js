ca68ff1bd05971ddf3568e26e51c0bf9bee3ca5a
PUSHRSP <pushrsp@gmail.com>
2023-02-18 21:27:28 +0900

    #12 - dashboard 페이지 BreadCrumb 컴포넌트 추가

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Box } from '@mui/material'
+
+import BreadCrumb from '../components/common/BreadCrumb'
 
 const Dashboard = () => {
-    return <div>Dashboard</div>
+    return (
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <BreadCrumb title="DASHBOARD" subtitle="Welcome to dashboard" />
+            </Box>
+        </Box>
+    )
 }
 
 export default Dashboard
